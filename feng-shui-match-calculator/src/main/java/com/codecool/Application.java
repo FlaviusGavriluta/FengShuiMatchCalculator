e0c90e495455294b6627ec146b4a5f3665afeae9
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-07-06 11:21:19 +0300

    implement FengShuiCalculatorUI

--- feng-shui-match-calculator/src/main/java/com/codecool/Application.java
+++ feng-shui-match-calculator/src/main/java/com/codecool/Application.java
@@ -1,12 +1,14 @@
 package com.codecool;
 
-import com.codecool.service.PersonaProvider;
-import com.codecool.service.PersonaProviderImpl;
+import com.codecool.service.*;
+import com.codecool.ui.FengShuiMatchCalculatorUi;
 
-class Program
-{
-    static void main(String[] args)
-    {
+class Program {
+    public static void main(String[] args) {
         PersonaProvider personaProvider = new PersonaProviderImpl();
+        SignCalculator signCalculator = new SignCalculatorImpl();
+        MatchCalculator matchCalculator = new MatchCalculatorImpl(signCalculator, personaProvider);
+        FengShuiMatchCalculatorUi ui = new FengShuiMatchCalculatorUi(matchCalculator);
+        ui.run();
     }
 }
--- feng-shui-match-calculator/src/main/java/com/codecool/ui/FengShuiMatchCalculatorUi.java
+++ feng-shui-match-calculator/src/main/java/com/codecool/ui/FengShuiMatchCalculatorUi.java
@@ -3,10 +3,13 @@ package com.codecool.ui;
 import com.codecool.model.Match;
 import com.codecool.service.MatchCalculator;
 
+import static java.lang.System.*;
+
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class FengShuiMatchCalculatorUi {
-
     private final MatchCalculator matchCalculator;
 
     public FengShuiMatchCalculatorUi(MatchCalculator matchCalculator) {
@@ -19,15 +22,22 @@ public class FengShuiMatchCalculatorUi {
         LocalDate birthdate2 = getBirthDate(2);
 
         Match match = matchCalculator.calculate(birthdate1, birthdate2);
-        System.out.println("Feng shui match result: " + match);
+        displayMatchResult(match);
     }
 
     private void displayWelcomeMessage() {
-        System.out.println("*** FENG SHUI MATCH CALCULATOR ***");
+        out.println("*** FENG SHUI MATCH CALCULATOR ***");
+    }
+
+    private void displayMatchResult(Match match) {
+        out.println("Feng Shui match result: " + match);
     }
 
     private LocalDate getBirthDate(int person) {
-        // Implement the method to get the birthdate for each person
-        return null;
+        Scanner scanner = new Scanner(in);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        out.println("Enter birthdate for Person " + " (yyyy-MM-dd): ");
+        String input = scanner.nextLine();
+        return LocalDate.parse(input, formatter);
     }
 }
